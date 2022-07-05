d0cefa8deaca76575fc515161e669f54402719e7
Yaroslav <yaroslaw6788@gmail.com>
2022-07-05 15:26:44 +0300

    processing POST request

--- services/urls.py
+++ services/urls.py
@@ -5,5 +5,5 @@ from services.views import ServicesListView
 app_name = "services"
 
 urlpatterns = [
-    path("services", ServicesListView.as_view(), name="services")
+    path("services/", ServicesListView.as_view(), name="services")
 ]
