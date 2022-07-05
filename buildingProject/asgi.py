7fbc874ee2349f62cab194299b17d0a2bc1208f7
Yaroslav <yaroslaw6788@gmail.com>
2022-07-05 15:32:24 +0300

    rename project

--- buildingProject/asgi.py
+++ buildingProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for leshaProject project.
+ASGI config for buildingProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leshaProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buildingProject.settings')
 
 application = get_asgi_application()
